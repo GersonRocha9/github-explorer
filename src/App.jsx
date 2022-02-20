8f829687267e2b751d664bc0018f7ed25378e34b
Gerson Rocha <gersonrocha9@gmail.com>
2022-02-20 13:01:51 -0300

    Create React App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Header from "./components/Header/index.jsx";
+
+import "./styles/global.scss";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <a href="/">
+          <p> Logo </p>
+        </a>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
