164fecbfc1ebf689215f3f0e760c191749f09446
Gerson Rocha <gersonrocha9@gmail.com>
2022-02-20 15:00:13 -0300

    API connected

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,32 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-  name: "Repo 1",
-  description: "Form in React Native",
-  link: "https://www.google.com.br",
-};
+import React, { useState, useEffect } from "react";
+
+import "../styles/repositories.scss";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  // Chamando a API do GitHub
+  useEffect(() => {
+    fetch("https://api.github.com/users/GersonRocha9/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
+      .catch((error) => console.log(error));
+  }, []);
+  // Array vazio no final para que a função seja executada apenas uma vez
+
+  console.log(repositories);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.id} repository={repository} />
+          // key = identificador unico para cada elemento
+        ))}
       </ul>
     </section>
   );
